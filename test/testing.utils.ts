1c5174b60854965c72a1da962baaf224e2319d16
Ilya <Ilya>
2017-06-13 14:48:47 +0400

    first

---
+++ test/testing.utils.ts
@@ -0,0 +1,51 @@
+// load some files
+import "core-js";
+import "zone.js/dist/zone";
+import "zone.js/dist/long-stack-trace-zone";
+import "zone.js/dist/proxy";
+import "zone.js/dist/sync-test";
+import "zone.js/dist/jasmine-patch";
+import "zone.js/dist/async-test";
+import "zone.js/dist/fake-async-test";
+
+import { TestBed } from "@angular/core/testing";
+import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from "@angular/platform-browser-dynamic/testing";
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template: `<h1>Hello {{name}}</h1>`
+})
+export class AppComponent { name = 'Angular'; }
+
+export interface TestEnvConfig {
+
+	services?: any[];
+	components?: any[];
+	imports?: any[];
+
+}
+
+export class TestingUtils {
+
+	private static initialized: boolean;
+
+	static initEnv(config: TestEnvConfig): void {
+
+		if (!this.initialized) {
+			TestBed.initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting());
+			this.initialized = true;
+		}
+		
+		TestBed.configureTestingModule({
+			providers: config.services,
+			declarations: config.components,
+			imports: config.imports
+		});
+	}
+
+	static component(component:any): any {
+		return TestBed.createComponent(component);
+	}
+
+}
